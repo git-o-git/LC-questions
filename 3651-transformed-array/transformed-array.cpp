7dd69628d10ee2bb0482d56e76fc17842184a3d0
Mansoor Alam <alalker789@gmail.com>
2026-02-05 14:20:32 +0530

    Time: 7 ms (54.87%) | Memory: 25.7 MB (39.55%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n);
+        vector<int> result(n);
 
         for(int i=0; i<n ; i++){
-            ans[i] = nums[((i + nums[i]) % n + n) % n];
+            result[i] = nums[((i + nums[i]) % n + n) % n];
         }
-        
-        return ans;
+
+        return result;
     }
 };
